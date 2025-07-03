1a2ca4dda1272acf3091b95f956b2e84e3cb9783
amalandsn <86369471+amalandsn@users.noreply.github.com>
2025-07-03 12:01:28 +0000

    dd

--- src/components/footer/CompanyInfo.tsx
+++ src/components/footer/CompanyInfo.tsx
@@ -35,7 +35,7 @@ const CompanyInfo = ({ content }: CompanyInfoProps) => {
         <img 
           src="/logof.webp"
           alt="Ndoukoumane Shipping & Services"
-          className="h-[100px] w-auto object-contain"
+          className="h-16 w-auto"
         />
         <div className="flex-1 min-w-0">
           <div className="text-xl font-bold font-serif leading-tight text-white">
